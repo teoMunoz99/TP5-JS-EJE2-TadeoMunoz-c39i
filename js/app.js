a876aedf1f446604973423a5fe49fe68ac2b11e3
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-04-04 01:28:06 -0300

    agregamos la estructura principal, la oja de estilos, el script con el objeto persona y una funcion para asignar la generacion

---
+++ js/app.js
@@ -0,0 +1,121 @@
+//Funciones
+function asignarGeneracion(anio) {
+
+    if (anio >= 1930 && anio <= 1948) {
+        document.write(`
+                <p>Pertenece a la generacion: Silent Generation</p>
+                <p>Rasgo característico: Austeridad</p>
+            `)
+    } else if (anio >= 1949 && anio <= 1968) {
+        document.write(`
+                <p>Pertenece a la generacion: Baby Boom</p>
+                <p>Rasgo característico: Ambición</p>
+            `)
+    } else if (anio >= 1969 && anio <= 1980) {
+        document.write(`
+                <p>Pertenece a la generacion: Generacion X</p>
+                <p>Rasgo característico: Obsesión por el éxito</p>
+            `)
+    } else if (anio >= 1981 && anio <= 1993) {
+        document.write(`
+                <p>Pertenece a la generacion: Generacion Y (Millenials)</p>
+                <p>Rasgo característico: Frustración</p>
+            `)
+    } else if (anio >= 1994 && anio <= 2010) {
+        document.write(`
+                <p>Pertenece a la generacion: Generacion Z</p>
+                <p>Rasgo característico: Irreverencia</p>
+            `)
+    }else {
+        document.write(`Todavía no pertenece a una generación`);
+    } 
+    
+}
+
+class Persona {
+    //declaro las propiedades privadas
+    #nombre;
+    #edad;
+    #dni;
+    #sexo;
+    #peso;
+    #altura;
+    #anioNacimiento;
+    //armo el constructor
+    constructor(nombre, edad, dni, sexo, peso, altura, anioNacimiento) {
+        this.#nombre = nombre;
+        this.#edad = edad;
+        this.#dni = dni;
+        this.#sexo = sexo;
+        this.#peso = peso;
+        this.#altura = altura;
+        this.#anioNacimiento = anioNacimiento;
+    }
+
+    //Getters
+
+    get nombre() {
+        return this.#nombre;
+    }
+
+    get edad() {
+        return this.#edad;
+    }
+
+    get dni() {
+        return this.#dni;
+    }
+    get sexo() {
+        return this.#sexo;
+    }
+    get peso() {
+        return this.#peso;
+    }
+    get altura() {
+        return this.#altura;
+    }
+    get anioNacimiento() {
+        return this.#anioNacimiento;
+    }
+
+    //Setters
+
+    set nombre(newNombre) {
+        this.#nombre = newNombre;
+    }
+
+    set edad(newEdad) {
+        this.#edad = newEdad;
+    }
+    set dni(newDni) {
+        this.#dni = newDni;
+    }
+    set sexo(newSexo) {
+        this.#sexo = newSexo;
+    }
+    set peso(newPeso) {
+        this.#peso = newPeso;
+    }
+    set altura(newAltura) {
+        this.#altura = newAltura;
+    }
+    set anioNacimiento(newAnioNacimiento) {
+        this.#anioNacimiento = newAnioNacimiento;
+    }
+
+    //Metodos
+
+    mostrarGeneracion() {
+        return asignarGeneracion(this.Persona.anioNacimiento);
+    }
+
+    esMayorDeEdad() {
+
+        if (this.Persona.edad >= 18) {
+            document.write(`Es mayor de edad`);
+        } else {
+            document.write(`Es menor de edad`);
+        }
+    }
+
+}
