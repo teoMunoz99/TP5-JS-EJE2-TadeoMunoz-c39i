61cb1947826b6f81cd18c142810a4e1276567fc2
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-04-04 16:05:14 -0300

    agregue la funcion y el boton que muestran los datos cargados

--- js/app.js
+++ js/app.js
@@ -73,123 +73,116 @@ class Persona {
     //Metodos
 
     mostrarGeneracion() {
-        if (this.anioNacimiento >= 1930 && this.anioNacimiento <= 1948) {
+
+        if (this.anioNacimiento === '') {
+            alert(`Debe completar el campo "Año de nacimiento"`);
+
+        } else if (this.anioNacimiento >= 1930 && this.anioNacimiento <= 1948) {
             alert(`
-                    <p>Pertenece a la generacion: Silent Generation</p>
-                    <p>Rasgo característico: Austeridad</p>
+                    Pertenece a la generacion: Silent Generation
+                    Rasgo característico: Austeridad
                 `);
         } else if (this.anioNacimiento >= 1949 && this.anioNacimiento <= 1968) {
             alert(`
-                    <p>Pertenece a la generacion: Baby Boom</p>
-                    <p>Rasgo característico: Ambición</p>
+                    Pertenece a la generacion: Baby Boom
+                    Rasgo característico: Ambición
                 `)
         } else if (this.anioNacimiento >= 1969 && this.anioNacimiento <= 1980) {
             alert(`
-                    <p>Pertenece a la generacion: Generacion X</p>
-                    <p>Rasgo característico: Obsesión por el éxito</p>
+                    Pertenece a la generacion: Generacion X
+                    Rasgo característico: Obsesión por el éxito
                 `)
         } else if (this.anioNacimiento >= 1981 && this.anioNacimiento <= 1993) {
             alert(`
-                    <p>Pertenece a la generacion: Generacion Y (Millenials)</p>
-                    <p>Rasgo característico: Frustración</p>
+                    Pertenece a la generacion: Generacion Y (Millenials)
+                    Rasgo característico: Frustración
                 `)
         } else if (this.anioNacimiento >= 1994 && this.anioNacimiento <= 2010) {
             alert(`
-                    <p>Pertenece a la generacion: Generacion Z</p>
-                    <p>Rasgo característico: Irreverencia</p>
+                    Pertenece a la generacion: Generacion Z
+                    Rasgo característico: Irreverencia
                 `)
-        }else {
+        } else {
             alert(`Todavía no pertenece a una generación`);
-        } 
+        }
     }
 
     esMayorDeEdad() {
 
-        if (this.edad >= 18) {
+        if (this.edad === '') {
+            alert(`Debe completar el campo "Edad"`);
+        } else if (this.edad >= 18) {
             alert(`Es mayor de edad`);
         } else {
             alert(`Es menor de edad`);
         }
     }
 
-    mostrarDatos(){
-        console.log(`
-        <ul>
-            <li>Nombre: ${this.nombre}</li>
-            <li>Edad: ${this.edad}</li>
-            <li>Dni : ${this.dni}</li>
-            <li>Sexo : ${this.sexo}</li>
-            <li>Peso : ${this.peso}</li>
-            <li>Altura : ${this.altura}</li>
-            <li>Año de nacimiento : ${this.anioNacimiento}</li>
-        </ul>`)
+    mostrarDatos() {
+
+        if (this.nombre === '') {
+            alert(`Debe completar todo el formulario`);
+        } else if (this.edad === '') {
+            alert(`Debe completar todo el formulario`);
+        } else if (this.dni === '') {
+            alert(`Debe completar todo el formulario`);
+        } else if (this.sexo === '') {
+            alert(`Debe completar todo el formulario`);
+        } else if (this.peso === '') {
+            alert(`Debe completar todo el formulario`);
+        } else if (this.altura === '') {
+            alert(`Debe completar todo el formulario`);
+        } else if (this.anioNacimiento === '') {
+            alert(`Debe completar todo el formulario`);
+        } else {
+            alert(`
+            
+                Nombre: ${this.nombre}
+                Edad: ${this.edad}
+                Dni : ${this.dni}
+                Sexo : ${this.sexo}
+                Peso : ${this.peso}Kg
+                Altura : ${this.altura}cm
+                Año de nacimiento : ${this.anioNacimiento}
+                
+            `)
+        }
     }
 }
 
-//Creo una sola vez un objeto persona para luego guardar o reemplazar ahi los datos ingredos
-let datos = new Persona('nombre','edad','dni','sexo','peso','altura','año de nacimiento');
+//Creo una sola vez un objeto persona para luego guardar ahi los datos ingredos
+let datos = new Persona('', '', '', '', '', '', '');
 
 //Funciones
 
 //creo que esta no la necesito al final
-function asignarGeneracion(anio) {
-
-    if (anio >= 1930 && anio <= 1948) {
-        alert(`
-                <p>Pertenece a la generacion: Silent Generation</p>
-                <p>Rasgo característico: Austeridad</p>
-            `);
-    } else if (anio >= 1949 && anio <= 1968) {
-        alert(`
-                <p>Pertenece a la generacion: Baby Boom</p>
-                <p>Rasgo característico: Ambición</p>
-            `)
-    } else if (anio >= 1969 && anio <= 1980) {
-        alert(`
-                <p>Pertenece a la generacion: Generacion X</p>
-                <p>Rasgo característico: Obsesión por el éxito</p>
-            `)
-    } else if (anio >= 1981 && anio <= 1993) {
-        alert(`
-                <p>Pertenece a la generacion: Generacion Y (Millenials)</p>
-                <p>Rasgo característico: Frustración</p>
-            `)
-    } else if (anio >= 1994 && anio <= 2010) {
-        alert(`
-                <p>Pertenece a la generacion: Generacion Z</p>
-                <p>Rasgo característico: Irreverencia</p>
-            `)
-    }else {
-        alert(`Todavía no pertenece a una generación`);
-    } 
-    
-}
 
 //con esta funcion guardo los datos que llegan del input
-function guardarDatos(e){
+function mostrarGen(e) {
     e.preventDefault();
-    datos.nombre = document.getElementById('inputNombre').value;
-    console.log(datos.nombre);
-    datos.edad = document.getElementById('inputEdad').value;
-    console.log(datos.edad);
-    datos.dni = document.getElementById('inputDni').value;
-    console.log(datos.dni);
-    datos.sexo = document.getElementById('inputSexo').value;
-    console.log(datos.sexo);
-    datos.peso = document.getElementById('inputPeso').value;
-    console.log(datos.peso);
-    datos.altura = document.getElementById('inputAltura').value;
-    console.log(datos.altura);
     datos.anioNacimiento = document.getElementById('inputAnioNacimiento').value;
     console.log(datos.anioNacimiento);
+    datos.mostrarGeneracion();
 }
 
-function mostrarGen(){
-    alert(datos.mostrarGeneracion());
+function mayEdad(e) {
+    e.preventDefault();
+    datos.edad = document.getElementById('inputEdad').value;
+    console.log(datos.edad);
+    datos.esMayorDeEdad();
 }
 
-function mayorEdad(){
-    alert(datos.esMayorDeEdad());
+function mostrarDat(e) {
+    e.preventDefault();
+    datos.nombre = document.getElementById('inputNombre').value;
+    datos.edad = document.getElementById('inputEdad').value;
+    datos.dni = document.getElementById('inputDni').value;
+    datos.sexo = document.getElementById('inputSexo').value;
+    datos.peso = document.getElementById('inputPeso').value;
+    datos.altura = document.getElementById('inputAltura').value;
+    datos.anioNacimiento = document.getElementById('inputAnioNacimiento').value;
+    datos.mostrarDatos();
+
 }
 
 //=======Creo que en esta parte tengo el problema==============//
@@ -197,13 +190,11 @@ function mayorEdad(){
 //despues que aparezcan los otros 2 mostrargen y mayoredad
 
 //traigo datos del formulario a la variable llamada formulario 
-let formulario = document.getElementById('formulario');
+let formulario = document.getElementById('btn-mostrarGeneracion');
+formulario.addEventListener('click', mostrarGen);
 
-//Agrego manejador de eventos
-formulario.addEventListener('submit', guardarDatos);
+let btnEdad = document.getElementById('btn-esMayorDeEdad');
+btnEdad.addEventListener('click', mayEdad);
 
-let btnGeneracion = document.getElementById('btn-cargarDatos');
-btnGeneracion.addEventListener('click', mostrarGen);
-
-let btnEdad = document.getElementById('btn-cargarDatos');
-btnEdad.addEventListener('click', mayorEdad);
+let btnDatos = document.getElementById('btn-mostrarDatos');
+btnDatos.addEventListener('click', mostrarDat);
